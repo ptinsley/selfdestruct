36f3dfc255c30a8a7b20cb7dca12c0d0d598e845
Paul Tinsley <paul.tinsley@gmail.com>
2019-07-01 15:20:34 -0500

    don't bind to localhost by default

--- server/server.go
+++ server/server.go
@@ -9,5 +9,5 @@ import (
 // Init - start up the server
 func Init() {
 	r := NewRouter()
-	r.Run(fmt.Sprintf("%s:%s", utils.EnvOr("LISTEN", "127.0.0.1"), utils.EnvOr("PORT", "8080")))
+	r.Run(fmt.Sprintf("%s:%s", utils.EnvOr("LISTEN", "0.0.0.0"), utils.EnvOr("PORT", "8080")))
 }
